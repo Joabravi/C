74af4ff74c90ffd2a9f98670d899ea3808a3a4f1
Joab Ramos <joab.ramos27@gmail.com>
2022-07-26 20:04:53 -0500

    Agregando mis primeros programas en c

---
+++ Variables/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    float y;
+    double y2;
+    char z;
+
+    x = 5;
+    y = 10.5;
+    y2 = 20.9;
+    z = 'a';
+
+    printf("Valor de x: %i.\n",x);
+    printf("Valor de y: %f.\n",y);
+    printf("Valor de y2: %f\n",y2);
+    printf("Valor de z: %c\n",z);
+
+    return 0;
+}
